bb5610ea9f30fb7c392978beb76767cc80a1e00a
Braian Mellor <braianj@gmail.com>
2023-07-18 16:36:31 -0300

    feat: loading design

--- src/components/Metadata/Metadata.tsx
+++ src/components/Metadata/Metadata.tsx
@@ -6,10 +6,11 @@ import Link from "decentraland-gatsby/dist/plugins/intl/Link"
 import Time from "decentraland-gatsby/dist/utils/date/Time"
 import TokenList from "decentraland-gatsby/dist/utils/dom/TokenList"
 import { Button } from "decentraland-ui/dist/components/Button/Button"
+import { arrayOf } from "prop-types"
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon"
 
 import UserMetadata from "./UserMetadata"
-import { Metadata } from "../../@types/image"
+import { Metadata, User } from "../../@types/image"
 import { SegmentImage } from "../../modules/segment"
 import { getExplorerUrl, getPlacesUrl } from "../../modules/utils"
 import LoadingText from "../Loading/LoadingText"
@@ -39,39 +40,47 @@ export default React.memo(function Metadata(props: MetadataProps) {
   return (
     <div className={TokenList.join(["metadata__container", className])}>
       <div className="metadata__wrapper">
-        <h1 className="metadata__title">{l("component.metadata.title")}</h1>
+        {!loading && (
+          <h1 className="metadata__title">{l("component.metadata.title")}</h1>
+        )}
+        {loading && <LoadingText type="p" size="medium" />}
         <div className="metadata__date">
           {!loading && Time(metadata.dateTime).utc().format("MMMM DD YYYY")}
           {loading && <LoadingText type="span" size="large" />}
         </div>
-        <h1 className="metadata__title">{l("component.metadata.place")}</h1>
-        <div className="metadata__place">
-          <Icon name="map marker alternate" />{" "}
-          {!loading && placeUrl && (
-            <Link href={placeUrl}>
-              {metadata.scene.name} - {metadata.scene.location.x},
-              {metadata.scene.location.y}
-            </Link>
-          )}
-          {!loading && !placeUrl && (
-            <span>
-              {metadata.scene.name} - {metadata.scene.location.x},
-              {metadata.scene.location.y}
-            </span>
-          )}
-          {loading && <LoadingText type="span" size="small" />}
-          <Button
-            primary
-            as="a"
-            href={jumpInUrl}
-            target="_blank"
-            data-event={SegmentImage.JumpIn}
-            data-scene={`${metadata.scene.location.x},${metadata.scene.location.y}`}
-            loading={loading}
-          >
-            {l("component.metadata.jump_in")}
-          </Button>
-        </div>
+        {!loading && (
+          <h1 className="metadata__title">{l("component.metadata.place")}</h1>
+        )}
+        {loading && <LoadingText type="p" size="small" />}
+        {!loading && (
+          <div className="metadata__place">
+            <Icon name="map marker alternate" />{" "}
+            {placeUrl && (
+              <Link href={placeUrl}>
+                {metadata.scene.name} - {metadata.scene.location.x},
+                {metadata.scene.location.y}
+              </Link>
+            )}
+            {!placeUrl && (
+              <span>
+                {metadata.scene.name} - {metadata.scene.location.x},
+                {metadata.scene.location.y}
+              </span>
+            )}
+            <Button
+              primary
+              as="a"
+              href={jumpInUrl}
+              target="_blank"
+              data-event={SegmentImage.JumpIn}
+              data-scene={`${metadata.scene.location.x},${metadata.scene.location.y}`}
+              loading={loading}
+            >
+              {l("component.metadata.jump_in")}
+            </Button>
+          </div>
+        )}
+        {loading && <LoadingText type="span" size="large" />}
       </div>
 
       <div className="metadata__divider" />
@@ -83,6 +92,18 @@ export default React.memo(function Metadata(props: MetadataProps) {
           <div className="metadata__user-wrapper">
             {metadata.visiblePeople.map((user) => (
               <UserMetadata key={user.userAddress} user={user} />
+            ))}
+          </div>
+        </div>
+      )}
+      {loading && (
+        <div className="metadata__people-container">
+          <h1 className="metadata__title-people">
+            <LoadingText type="p" size="small" />
+          </h1>
+          <div className="metadata__user-wrapper">
+            {Array.from(Array(3), (_, key) => (
+              <UserMetadata key={key} user={{} as User} loading />
             ))}
           </div>
         </div>
