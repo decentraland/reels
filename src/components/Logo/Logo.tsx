f07ceab31722abc9e616830cbe3bb99c722ec966
Braian Mellor <braianj@gmail.com>
2023-08-04 15:02:48 -0300

    feat: add link to dcl logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,20 @@
 import React from "react"
 
+import Link from "decentraland-gatsby/dist/plugins/intl/Link"
 import DclLogo from "decentraland-gatsby/static/decentraland.svg"
 
 import DclText from "../../images/dcl-text.svg"
 
 import "./Logo.css"
 
+const DECENTRALAND_HOMEPAGE_URL =
+  process.env.GATSBY_DECENTRALAND_HOMEPAGE_URL || "https://decentraland.org"
+
 export default React.memo(function Logo() {
   return (
-    <div className="logo-container">
+    <Link href={DECENTRALAND_HOMEPAGE_URL} className="logo-container">
       <img className="logo-image" src={DclLogo} />
       <img className="logo-text" src={DclText} />
-    </div>
+    </Link>
   )
 })
